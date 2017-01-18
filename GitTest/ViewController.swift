b96a45fa9a06a588b1dfd5d93dad8dff11a7baa6
JoeboJiang <jiubai.jiang@kodak.com>
2017-01-18 15:28:29 +0800

    Ff

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        //aaaa 
+        //aaaa  
         // Do any additional setup after loading the view, typically from a nib.
     }
 
