a1a3934fe7f2426e0550c50c9e25a83ab5cf9107
joebojiang <751867978@qq.com>
2017-02-07 16:13:16 +0800

    joebo

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() { 
         super.viewDidLoad()
+        //joebo
         //hello world
         //fix bugs
         //modify at master branch
